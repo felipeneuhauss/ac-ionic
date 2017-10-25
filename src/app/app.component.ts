2e5ea2261afad2c48772cff88a78ec03854656fc
Felipe Neuhauss <felipe.neuhauss@gmail.com>
2017-10-25 11:21:47 -0200

    UniqueDeviceId added to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { LoginPage } from "../pages/login/login";
 import { AuthProvider } from "../providers/auth/auth";
 import { TouchID } from '@ionic-native/touch-id';
 import { ApiProvider } from '../providers/api/api';
+import { UniqueDeviceID } from '@ionic-native/unique-device-id';
 
 @Component({
   templateUrl: 'app.html'
@@ -26,7 +27,7 @@ export class MyApp implements OnInit {
 
   constructor(public platform: Platform, public statusBar: StatusBar, private touchId: TouchID,
               public splashScreen: SplashScreen, private auth : AuthProvider, private api: ApiProvider,
-              private alertCtrl: AlertController) {
+              private alertCtrl: AlertController, private uniqueDeviceID: UniqueDeviceID) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -82,18 +83,23 @@ export class MyApp implements OnInit {
         (res) => {
             console.log(res);
             this.touchIdResponse = res;
-            this.api.post('users/touch-id',
-                {username: this.auth.user().username, touch_id: JSON.stringify(res)}).then(
-            (res) => {
-                console.log('Ok, finger print saved');
-                const alert = this.alertCtrl.create({
-                    title: 'Sucesso!',
-                    subTitle: 'Touch ID vinculado com sucesso!',
-                    buttons: ['OK']
-                });
-                alert.present();
-            });
-            console.log('Ok', res)
+            this.uniqueDeviceID.get()
+                .then((uuid: any) => {
+                    console.log('uuid', uuid)
+                    this.api.post('users/touch-id',
+                        {username: this.auth.user().username, touch_id: uuid}).then(
+                        (res) => {
+                            console.log('Ok, finger print saved');
+                            const alert = this.alertCtrl.create({
+                                title: 'Sucesso!',
+                                subTitle: 'Touch ID vinculado com sucesso!',
+                                buttons: ['OK']
+                            });
+                            alert.present();
+                        });
+                })
+                .catch((error: any) => console.log(error));
+
         },
         (err) => {
             this.touchIdResponse = err;
